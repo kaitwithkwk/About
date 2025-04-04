288919c7ac8ed2887ca5d0bf8b29e1a7f3efaacf
Scholar Name <scholar@kodewithklossy.com>
2025-04-04 17:24:25 -0500

    Initial Commit

---
+++ About/AboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutApp.swift
+//  About
+//
+//  Created by Scholar on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
