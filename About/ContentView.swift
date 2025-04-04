963ee2e028e46fd3081a2c526acd5eba0a4bdec7
Scholar Name <scholar@kodewithklossy.com>
2025-04-04 18:08:58 -0500

    Complete iteration 3 with modifiers

--- About/ContentView.swift
+++ About/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.gray.opacity(0.1)
+            Color.teal.opacity(0.5)
                 .ignoresSafeArea()
             ScrollView {
                 VStack(spacing: 20) {
@@ -19,7 +19,10 @@ struct ContentView: View {
                         .frame(width:230, height: 350)
                         .clipShape(Circle())
                     Text("Kaitlyn Van Gorkom")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
                     Text("Senior Manager of Curriculum and Program Design")
+                        .font(.headline)
                         .multilineTextAlignment(.center)
                     VStack {
                         Text("I’ve been with the KWK team for about two months now, but it feels a lot longer than that because my first introduction to the KWK community was back in 2020 as an instructor. Outside of work, I love traveling to new places and salsa dancing!")
@@ -27,6 +30,9 @@ struct ContentView: View {
                     }
                     .background(Color.white)
                     .cornerRadius(20)
+                    .frame(
+                        maxWidth: 0.85 * UIScreen.main.bounds.width
+                    )
                     VStack {
                         Image("nyc")
                             .resizable()
@@ -34,6 +40,9 @@ struct ContentView: View {
                             .frame(width: 350, height: 300)
                             .clipped()
                     }
+                    .frame(
+                        maxWidth: 0.85 * UIScreen.main.bounds.width
+                    )
                     .cornerRadius(20)
                     VStack (alignment: .leading) {
                         Text("These are a few of my favorite things...\n - Coffee\n - Long walks \n - Sunsets")
@@ -41,9 +50,7 @@ struct ContentView: View {
                             .padding()
                     }
                     .frame(
-                        minWidth: 0,
-                        maxWidth: 0.88 * UIScreen.main.bounds.width
-                        
+                        maxWidth: 0.85 * UIScreen.main.bounds.width
                     )
                     .background(Color.white)
                     .cornerRadius(20)
